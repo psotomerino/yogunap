a6fb11516d53947db26146a3e30318c213f12c2e
Patricio Soto <psotomerino@gmail.com>
2022-09-07 05:51:29 -0500

    sin_conexion

--- backend/conexion.php
+++ backend/conexion.php
@@ -1,6 +1,6 @@
 <?php 
 function getConn(){
-  $mysqli = mysqli_connect('localhost', 'jirehedu', 'Pasm.2021jc', "jirehedu_master");
+  $mysqli = mysqli_connect('', '', '', "");
   if (mysqli_connect_errno($mysqli))
     echo "Fallo al conectar a MySQL: " . mysqli_connect_error();
   $mysqli->set_charset('utf8');
